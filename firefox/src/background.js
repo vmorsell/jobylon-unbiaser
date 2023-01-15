35ce29394861ea0a18ba2917947aabfb618ef36a
Viktor Mörsell <viktor@upper.st>
2023-01-15 11:21:13 +0100

    Prettify

--- firefox/src/background.js
+++ firefox/src/background.js
@@ -3,22 +3,22 @@
 let registered = {};
 
 async function registerScript(message) {
-    const id = message.id;
-    const hosts = message.hosts;
-    const css = message.css;
-    const enabled = message.enabled;
+  const id = message.id;
+  const hosts = message.hosts;
+  const css = message.css;
+  const enabled = message.enabled;
 
-    if (!enabled) {
-        registered[id].unregister();
-        return;
-    }
+  if (!enabled) {
+    registered[id].unregister();
+    return;
+  }
 
   const unregister = await browser.contentScripts.register({
     matches: hosts,
-    css: [{file: css}],
-    runAt: "document_start"
+    css: [{ file: css }],
+    runAt: "document_start",
   });
-    registered[id] = unregister;
+  registered[id] = unregister;
 }
 
 browser.runtime.onMessage.addListener(registerScript);
--- firefox/src/unbiaser.js
+++ firefox/src/unbiaser.js
@@ -4,27 +4,27 @@
  * Define available unbiasing filters.
  */
 const filters = {
-    picture: {
-        label: "Profile picture",
-        css: "/content_scripts/picture.css",
-    },
-    multipleApplications: {
-        label: "Multiple applications",
-        css: "/content_scripts/multiple_applications.css",
-    },
-    alva: {
-        label: "Alva results",
-        css: "/content_scripts/alva.css",
-    },
-    layke: {
-        label: "Layke matching",
-        css: "/content_scripts/layke.css",
-    },
-    answers: {
-        label: "Answers",
-        css: "/content_scripts/answers.css",
-    },
-}
+  picture: {
+    label: "Profile picture",
+    css: "/content_scripts/picture.css",
+  },
+  multipleApplications: {
+    label: "Multiple applications",
+    css: "/content_scripts/multiple_applications.css",
+  },
+  alva: {
+    label: "Alva results",
+    css: "/content_scripts/alva.css",
+  },
+  layke: {
+    label: "Layke matching",
+    css: "/content_scripts/layke.css",
+  },
+  answers: {
+    label: "Answers",
+    css: "/content_scripts/answers.css",
+  },
+};
 
 /*
  * Set the URL we operate for.
@@ -40,21 +40,21 @@ const url = "*://emp.jobylon.com/*";
 const storageKey = "state";
 
 function getState() {
-    const state = JSON.parse(localStorage.getItem(storageKey));
-    if (state === null) {
-        // First-time use. Return a state where all functionality is visible.
-        let s = {};
-        for (var k in filters) {
-            s[k] = true;
-        }
-        saveState(s);
-        return s;
+  const state = JSON.parse(localStorage.getItem(storageKey));
+  if (state === null) {
+    // First-time use. Return a state where all functionality is visible.
+    let s = {};
+    for (var k in filters) {
+      s[k] = true;
     }
-    return state;
-} 
+    saveState(s);
+    return s;
+  }
+  return state;
+}
 
 function saveState(state) {
-    localStorage.setItem(storageKey, JSON.stringify(state));
+  localStorage.setItem(storageKey, JSON.stringify(state));
 }
 
 /*
@@ -63,31 +63,31 @@ function saveState(state) {
 let state = getState();
 
 for (var id in filters) {
-    const cb = document.createElement("input")
-    cb.setAttribute("type", "checkbox");
-    cb.setAttribute("id", id);
-    const checked = state[id]
-    if (checked) {
-        cb.checked = true;
-    }
-    cb.addEventListener("change", async (e) => {
-        browser.runtime.sendMessage({
-            id: e.target.id,
-            hosts: [url],
-            css: filters[e.target.id].css,
-            enabled: !e.target.checked,
-        });
+  const cb = document.createElement("input");
+  cb.setAttribute("type", "checkbox");
+  cb.setAttribute("id", id);
+  const checked = state[id];
+  if (checked) {
+    cb.checked = true;
+  }
+  cb.addEventListener("change", async (e) => {
+    browser.runtime.sendMessage({
+      id: e.target.id,
+      hosts: [url],
+      css: filters[e.target.id].css,
+      enabled: !e.target.checked,
+    });
 
-        state[e.target.id] = e.target.checked;
-        saveState(state);
-    })
+    state[e.target.id] = e.target.checked;
+    saveState(state);
+  });
 
-    const lb = document.createElement("label")
-    lb.setAttribute("for", id);
-    lb.append(filters[id].label);
+  const lb = document.createElement("label");
+  lb.setAttribute("for", id);
+  lb.append(filters[id].label);
 
-    const form = document.getElementById("form");
-    form.appendChild(cb);
-    form.appendChild(lb);
-    form.appendChild(document.createElement("br"));
+  const form = document.getElementById("form");
+  form.appendChild(cb);
+  form.appendChild(lb);
+  form.appendChild(document.createElement("br"));
 }
