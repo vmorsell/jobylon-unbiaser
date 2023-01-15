641b32cf0ccd495b307cca7ed7b9015d2c9a3641
Viktor Mörsell <viktor@upper.st>
2023-01-15 14:15:42 +0100

    Use four spaces indentation

--- src/background.js
+++ src/background.js
@@ -1,24 +1,24 @@
-"use strict";
+'use strict';
 
 let registered = {};
 
 async function registerScript(message) {
-  const id = message.id;
-  const hosts = message.hosts;
-  const css = message.css;
-  const enabled = message.enabled;
+    const id = message.id;
+    const hosts = message.hosts;
+    const css = message.css;
+    const enabled = message.enabled;
 
-  if (!enabled) {
-    registered[id].unregister();
-    return;
-  }
+    if (!enabled) {
+        registered[id].unregister();
+        return;
+    }
 
-  const unregister = await browser.contentScripts.register({
-    matches: hosts,
-    css: [{ file: css }],
-    runAt: "document_start",
-  });
-  registered[id] = unregister;
+    const unregister = await browser.contentScripts.register({
+        matches: hosts,
+        css: [{ file: css }],
+        runAt: 'document_start',
+    });
+    registered[id] = unregister;
 }
 
 browser.runtime.onMessage.addListener(registerScript);
